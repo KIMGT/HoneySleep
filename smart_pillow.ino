6a60259b48e3ec00e4a3dda3fc8e007a5933778c
gomtings <33356618+gomtings@users.noreply.github.com>
2018-11-17 02:15:57 +0900

    Update smart_pillow.ino

--- smart_pillow.ino
+++ smart_pillow.ino
@@ -1,6 +1,13 @@
-#include <SoftwareSerial.h>
-SoftwareSerial bt(2, 3);   //bluetooth module Tx:Digital 3 Rx:Digital 2
-/*co2 sansor setup 부분 입니다. */
+#include <SoftwareSerial.h> //  
+#include <Wire.h> //I2C 통신용 헤더 파일 입니다. 
+#include "MAX30100_PulseOximeter.h" // 심박,산소포화도 센서 라이브러리 입니다.
+SoftwareSerial bt(3, 4);   //bluetooth module Tx:Digital 3 Rx:Digital 2
+/*산소포화도 센서 setup 부 입니다.*/
+#define REPORTING_PERIOD_MS     1000
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
+void onBeatDetected(){ Serial.println("Beat!");}
+/*co2 sansor 부분 입니다. */
 #define MG_PIN (A0) //define which analog input channel you are going to use – A0 pin
 #define BOOL_PIN (9)    // D9 – Dout pin
 #define DC_GAIN (8.5) //define the DC gain of amplifier
@@ -20,12 +27,67 @@ float CO2Curve[3] = {2.602,ZERO_POINT_VOLTAGE,(REACTION_VOLTGAE/(2.602-3))};
 //data format:{ x, y, slope}; point1: (lg400, 0.324), point2: (lg4000, 0.280)
 //slope = ( reaction voltage ) / (log400 –log1000)
 void setup(){
-Serial.begin(9600); //UART setup, baudrate = 9600bps
+  Serial.begin(115200); //UART setup, baudrate = 9600bps ,115200
+   /*산소포화도 센서 입니다. */
+    Serial.print("Initializing pulse oximeter..");
+    // Initialize the PulseOximeter instance
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+      //pox.setOnBeatDetectedCallback(onBeatDetected);
+  /*co2 sansor setup 부 입니다. */
 pinMode(BOOL_PIN, INPUT); //set pin to input
 digitalWrite(BOOL_PIN, HIGH); //turn on pullup resistors
 Serial.print("MG-811 Demostration\n");
 }
-void loop(){
+void loop(){// roop 문
+  
+  Heart();
+  co2();
+delay(200);
+
+
+}
+float MGRead(int mg_pin){
+int i;
+float v=0;
+for (i=0;i<READ_SAMPLE_TIMES;i++) {
+v += analogRead(mg_pin);
+delay(READ_SAMPLE_INTERVAL);
+}
+v = (v/READ_SAMPLE_TIMES) *5/1024 ;
+return v;
+}
+int MGGetPercentage(float volts, float *pcurve){
+if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
+return -1;
+} else {
+return pow(10, ((volts/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
+}
+}
+void Heart(){
+    /*   산소포화도 센서 루프 문 입니다.*/
+    // Make sure to call update as fast as possible
+    pox.update();
+
+    // Asynchronously dump heart rate and oxidation levels to the serial
+    // For both, a value of 0 means "invalid"
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("Heart rate:");
+        Serial.print(pox.getHeartRate());
+        Serial.print("bpm / SpO2:");
+        Serial.print(pox.getSpO2());
+        Serial.println("%");
+        tsLastReport = millis();
+    }
+}
+void co2(){
+    /*co2 센서 입니다.*/
 int percentage;
 float volts;
 volts = MGRead(MG_PIN);
@@ -48,22 +110,5 @@ Serial.print( "=====BOOL is HIGH======" );
 Serial.print( "=====BOOL is LOW======" );
 }
 Serial.print("\n");
-delay(200);
 }
-float MGRead(int mg_pin){
-int i;
-float v=0;
-for (i=0;i<READ_SAMPLE_TIMES;i++) {
-v += analogRead(mg_pin);
-delay(READ_SAMPLE_INTERVAL);
-}
-v = (v/READ_SAMPLE_TIMES) *5/1024 ;
-return v;
-}
-int MGGetPercentage(float volts, float *pcurve){
-if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
-return -1;
-} else {
-return pow(10, ((volts/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
-}
-}
+
